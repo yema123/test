b734b1cdfa221aef3636279f8ba7c437afe4eb2c
yema123 <1109870828@qq.com>
2021-11-03 00:30:21 +0800

    测试本地提交

--- src/cn/test/AA.java
+++ src/cn/test/AA.java
@@ -1,4 +1,5 @@
 package cn.test;
 
 public class AA {
+
 }
---
+++ src/cn/test/HelloWold.java
@@ -0,0 +1,7 @@
+package cn.test;
+
+public class HelloWold {
+    public static void main(String[] args) {
+        System.out.println("hello wold2ssss");
+    }
+}
